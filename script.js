941cd525632e6e6828c33d29d918f1b3de193d86
Kirk <blowme@cow.com>
2021-02-03 13:32:02 -0700

    wired up object creation and other improvements

--- script.js
+++ script.js
@@ -2,73 +2,92 @@
 // App to connect to an embedded device (like SparkFun OpenLog) and manage settings via BLE
 
 
-// Define the Target BLE Service
+// SETUP - Define the Target BLE Service
 //--------------------------------------------------------------------------------------
-var uuidTargetService = "4fafc201-1fb5-459e-8fcc-c5c9c331914b";
+const kTargetServiceUUID = "4fafc201-1fb5-459e-8fcc-c5c9c331914b";
+const kTargetServiceName = "OpenLog";
 //--------------------------------------------------------------------------------------
 
 console.clear();
-// set the body to full height
-// document.body.style.height = `${innerHeight}px`
 
-
-// function that handles the checkboxes state, so at least one needs to be selected. The last checkbox will be disabled.
-function disableOnlyCheckbox(){
-	let totalChecked = [uppercaseEl, lowercaseEl, numberEl, symbolEl].filter(el => el.checked)
-	totalChecked.forEach(el => {
-		if(totalChecked.length == 1){
-			el.disabled = true;
-		}else{
-			el.disabled = false;
-		}
-	})
-}
 //--------------------------------------------------------------------------------------
 // Property Creation and Management
 //
 // Dynamically create and manage properties
 //--------------------------------------------------------------------------------------
 
+// BLE Codes for our service
+const kBLEDescCharNameUUID = 0x2901;
+const kBLEDescSFEPropTypeUUID = 0xA101;
+
+// Property type codes - sent as a value of the char descriptor 
+const kSFEPropTypeBool      = 0x1;
+const kSFEPropTypeInt       = 0x2;
+const kSFEPropTypeRange     = 0x3;
+const kSFEPropTypeText      = 0x4;
+
+
+// Simple Data marshling code
+
+const textDecoder = new TextDecoder();
+function dataToText(data){ return textDecoder.decode(data);}
+
 // KDB HACKS
-	const targetID = 'settings-container';
-	const properties=[];
+	
+const targetID = 'settings-container';
+const properties=[];
 
-	let namecnt = 0;
-	class Property{
+function setDeviceName(name){
+    document.getElementById("settings-title").innerHTML= name+ " Settings";
+}
+// Used to main unique ids for property objects
+let namecnt = 0;
 
-    	constructor(bleChar){
-          	this.characteristic = bleChar;
-        	this.ID = "property-" + namecnt++;
-    	}
+// Base property class 
+class Property{
 
+    constructor(bleChar){
+      	this.characteristic = bleChar;
+    	this.ID = "property-" + namecnt++;  // unique id for the DOM
+    }
 
-    	getID(){return this.ID;}
-	}
+    init(){
+
+        // get the name of this prop
+        this.characteristic.getDescriptor(kBLEDescCharNameUUID).then(desc =>{
+
+            desc.readValue().then(value =>{
+                // decode name, set in instance data
+                this.name = dataToText(value);
+
+                // We have the name - call method to generate UX and complete setup
+                this.generateElement();
+// TODO ERROR HANDLING
+            });
+        });
+
+    }
+
+    generateElement(){} // stub
+}
 	// -------------------------------------------
   	class boolProperty extends Property{
 
    		generateElement(){
 
-
-   			// GET title from the BLE characterisitic
-   			//TODO
-   			let name ="Testing Value";
-			// TODO -- put most of this in the object
    			let div = document.createElement("div");
-   			let ID = this.getID();
-   			console.log(ID);
+
    			div.innerHTML = `
 	   			<div class="setting">
-					<input type="checkbox" id="`+ID + `" />
-					<label for="` + ID + `">` + name + `</label>
+					<input type="checkbox" id="`+ this.ID + `" />
+					<label for="` + this.ID + `">` + this.name + `</label>
 				</div>
    			`;
 
    			document.getElementById(targetID).appendChild(div);
-
    			this.inputField = div.querySelector('input');
 
-
+            // event handler wireup - on change event, save the new value to BLE device
    			this.inputField.addEventListener("change", () => {
    				this.saveValue();
    			});
@@ -97,28 +116,30 @@ function disableOnlyCheckbox(){
    		newProperty.generateElement(); // generate the HTML
    }
 
+   //-------------------------------------------------------------
 	const range_fill = "#0B1EDF";
 	const range_background = "rgba(255, 255, 255, 0.214)";   
 
   	class rangeProperty extends Property{
 
+        int(){
+            // Get Min and Max of Range
+            //TODO
+
+            this.min = 0;
+            this.max = 200;
+
+            // call super to finish setup
+            super.init();            
+        }
+        //------------------------
    		generateElement(){
 
-
-   			// GET title from the BLE characterisitic
-   			//TODO
-   			let name ="Testing Value";
-   			let min = 0;
-   			let max = 200;
-
-			// TODO -- put most of this in the object
    			let div = document.createElement("div");
-   			let ID = this.getID();
-   			console.log(ID);
    			div.innerHTML = `
-   				<div class="length range__slider" data-min="`+min+`" data-max="`+max+`">
-					<div class="length__title field-title" data-length='0'>`+name+`:</div>
-					<input id="slider" type="range" min="`+min+`" max="`+max+`" value="16" />
+   				<div class="length range__slider" data-min="`+ this.min +`" data-max="` + this.max +`">
+					<div class="length__title field-title" data-length='0'>`+ this.name +`:</div>
+					<input id="slider" type="range" min="`+ this.min +`" max="`+ this.max +`" value="16" />
 				</div>
    			`;
 			this.input = div.querySelector('input');
@@ -160,7 +181,6 @@ function disableOnlyCheckbox(){
    }
 
 	function addRangeProperty(){
-
   
    		let char = "dummy"; // image this being a ble characteristic
    		
@@ -173,25 +193,16 @@ function disableOnlyCheckbox(){
 
    		generateElement(){
 
-
-   			// GET title from the BLE characterisitic
-   			//TODO
-   			let name ="A Text Value";
-			// TODO -- put most of this in the object
    			let div = document.createElement("div");
-   			let ID = this.getID();
-   			console.log(ID);
    			div.innerHTML = `
 	   			<div class="text-prop">
-					<input type="text" id="`+ID + `" />
-					<label for="` + ID + `">` + name + `</label>
+					<input type="text" id="`+ this.ID + `" />
+					<label for="` + this.ID + `">` + this.name + `</label>
 				</div>
    			`;
    			document.getElementById(targetID).appendChild(div);
 
    			this.inputField = div.querySelector('input');
-
-
    			this.inputField.addEventListener("change", () => {
    				this.saveValue();
    			});
@@ -230,25 +241,16 @@ function disableOnlyCheckbox(){
 
    		generateElement(){
 
-
-   			// GET title from the BLE characterisitic
-   			//TODO
-   			let name ="A Integer Value";
-			// TODO -- put most of this in the object
    			let div = document.createElement("div");
-   			let ID = this.getID();
-   			console.log(ID);
    			div.innerHTML = `
 	   			<div class="text-prop">
-					<input type="text" id="`+ID + `" onkeypress="return isNumberKey(event)"/>
-					<label for="` + ID + `">` + name + `</label>
+					<input type="text" id="`+ this.ID + `" onkeypress="return isNumberKey(event)"/>
+					<label for="` + this.ID + `">` + this.name + `</label>
 				</div>
    			`;
    			document.getElementById(targetID).appendChild(div);
 
    			this.inputField = div.querySelector('input');
-
-
    			this.inputField.addEventListener("change", () => {
    				this.saveValue();
    			});
@@ -277,35 +279,62 @@ function disableOnlyCheckbox(){
    		newProperty.generateElement(); // generate the HTML
    }
 
+// Our property object defs = KEY: The order is same as type code index above. 
+const propFactory = [boolProperty, intProperty, rangeProperty, textProperty];
+
+//--------------------------------------------------------------------------------------
+// Add a property to the system based on a BLE Characteristic
+//
+function addPropertyToSystem(bleCharacteristic){
+
+    // Get the type descriptor
+    bleCharacteristic.getDescriptor(kBLEDescSFEPropTypeUUID).then(desc =>{
+
+        // Get the value of the type descriptor
+        desc.readValue().then(value =>{
+            let type = value.getUint8(0,0);
+
+            console.log("Descriptor Property Type: " + type);
+
+            if(type < kSFEPropTypeBool || type > kSFEPropTypeText ){
+                console.log("Invalid Type value: " + type);
+                return;
+            }
+            // build prop object - notice index into array of prop class defs 
+            let property = new propFactory[type-1](bleCharacteristic);            
+            properties.push(property);
+            // init our property
+            property.init();
+
+        });
+    });
+}
 //--------------------------------------------------------------------------------------
 // BLE Logic
 //--------------------------------------------------------------------------------------
 function connectToBLEService() {
 
-
-  
     let filters = [];
     // KDB - for esp32 ...
     // Filtering on the name on it's own will find the device, but not the device with the
     // desired service. So adding the service to the search will find a device with the service
     // BUT not the desired name (at least in the returned objects ...)
-    filters.push({name: "OpenLog"}); 
-    filters.push({services:[uuidTargetService]});
+    filters.push({name: kTargetServiceName}); 
+    filters.push({services:[kTargetServiceUUID]});
     let options = {};
     options.filters = filters;
 
-   
-
     return navigator.bluetooth.requestDevice(options).then(device => {
         
-        console.log("Device Name: " + device.name);
+        if(device.name)
+            setDeviceName(device.name);
 
         return device.gatt.connect().then(gattServer => {
             console.log("Connected to gattServer");
             console.log(gattServer);
             
             // Connect to our target Service 
-            gattServer.getPrimaryService(uuidTargetService).then(primaryService => {
+            gattServer.getPrimaryService(kTargetServiceUUID).then(primaryService => {
                 console.log("Connected-> Primary Service");
                 console.log(primaryService);
 
@@ -313,35 +342,11 @@ function connectToBLEService() {
                 primaryService.getCharacteristics().then(theCharacteristics => {                
 
                     console.log("Service Characteristics");
+
+                    // Add the characteristics to the property sheet
                     for(const aChar of theCharacteristics){
-                        console.log(aChar);
-                        aChar.getDescriptor(0x2901).then(desc =>{
-                            console.log("Retrieved Descriptor");
-                            desc.readValue().then(value =>{
-                                console.log(value);
-                                let enc = new TextDecoder();
-                                let name = enc.decode(value);
-
-                                console.log(name);
-
-                                // Lets get the type of this thing
-                                aChar.getDescriptor(0xA101).then(desc => {
-                                    desc.readValue().then(value =>{
-                                        console.log(value);
-                                        let type = value.getUint8(0,0);
-
-                                        console.log("Char " + name + ", Type: " + type);
-
-                                        if(type == 1){
-                                            addBoolProperty();
-                                        }
-                                    });
-                                });
-                            })
-
-                        });
+                        addPropertyToSystem(aChar);                        
                     }
-                    console.log(theCharacteristics.map(c => c.uuid).join('\n' + ' '.repeat(10)));
 
                 }).catch(error => {
                     console.log("getCharacteristics error");
