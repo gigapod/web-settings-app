ca26e931101689cea8fe569bc00f9d78a8e4f6e7
Kirk <blowme@cow.com>
2021-02-10 07:05:08 -0700

    cleanup of example code

--- firmware/Artemis_BLE_Props/Artemis_BLE_Props.ino
+++ firmware/Artemis_BLE_Props/Artemis_BLE_Props.ino
@@ -1,23 +1,27 @@
 
-
+/*
+ *
+ * SparkFun ArduinoBLE (SparkFun Artemis) BLE Settings App Example
+ * =======================================
+ *
+ * [TODO - Descripton, attribution and license]
+ *
+ */
 #include "ArduinoBLE.h"
 
+// Include SparkFun functions to define "properties" out of characteristics
+// This enables use of the SparkFun BLE Settings Web-App
 #include "sf_ble_prop.h"
 
+// See the following for generating UUIDs:
+// https://www.uuidgenerator.net/
 #define kTargetServiceUUID  "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define kTargetServiceName  "Artemis App"
 
-
 //--------------------------------------------------------------------------------------
-
-//--------------------------------------------------------------------------------------
-// Property Creation and Management
-//
-// Dynamically create and manage properties
-//--------------------------------------------------------------------------------------
-
-
 // Our Characteristic UUIDs - and yes, just made these up
+//--------------------------------------------------------------------------------------
+
 #define kCharacteristicBaudUUID     "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 #define kCharacteristicEnabledUUID  "beb5483e-36e1-4688-b7f5-ea07361b26a9"
 #define kCharacteristicMessageUUID  "beb5483e-36e1-4688-b7f5-ea07361b26aa"
@@ -26,27 +30,38 @@
 #define kCharacteristicTimeUUID     "beb5483e-36e1-4688-b7f5-ea07311b260d"
 #define kCharacteristicOffsetUUID   "beb5483e-36e1-4688-b7f5-ea07311b260e"
 
-// helper
+// helper for message limits
 #define kMessageMax 64
 
-// Declare the main objects that define this BLE service here. Will connect everything later
-// This is a service with four characteristics
+//--------------------------------------------------------------------------------------
+// ArduinoBLE Object declaration 
+//
+// Declare the main objects that define this BLE service here. Will connect everything 
+// together late in the app logic. This enables these objects to be stack based
 
-// Service
+// Our BLE Service
 BLEService bleService(kTargetServiceUUID);
+
+// Define the BLE Service Characteristics - or "Properties"
+//
+// Note - for each characterisitic, a storage value is defined for it
+//
+// >> Notifications <<
+//    Only enable for those characteristics that will use notifications. Adding this type
+//    to a Characteristic adds a BLE Descriptor. If not using notify, save the resources.
 
 // First Characteristic - "Enabled" - bool - is the system enabled 
 boolean bIsEnabled = true;
-BLEBooleanCharacteristic bleCharEnabled(kCharacteristicEnabledUUID, BLERead | BLENotify | BLEWrite);
+BLEBooleanCharacteristic bleCharEnabled(kCharacteristicEnabledUUID, BLERead | BLEWrite);
 
 // Second Characteristic - Baud Rate - int - the baud rate of the syste
 int32_t baudRate = 115200;
-BLEIntCharacteristic bleCharBaudRate(kCharacteristicBaudUUID, BLERead | BLENotify | BLEWrite);
+BLEIntCharacteristic bleCharBaudRate(kCharacteristicBaudUUID, BLERead | BLEWrite);
 
-// Third Characteristic - Message - string - a string message for the service
-String strMessage = "Welcome";
+// Third Characteristic - Device Name - string - a string Name for the device
+String strName = "Artimis Device";
 // Note: Setting max value size of kMessage Max  
-BLEStringCharacteristic bleCharMessage(kCharacteristicMessageUUID, BLERead | BLENotify | BLEWrite, kMessageMax);
+BLEStringCharacteristic bleCharName(kCharacteristicMessageUUID, BLERead | BLEWrite, kMessageMax);
 
 // Fourth Characteristic - Sample Rate - Range (int) - how often to sample this thing
 uint32_t sampleRate = 123;
@@ -54,20 +69,19 @@ uint32_t sampleRate = 123;
 const uint32_t sampleRateMin = 10;
 const uint32_t sampleRateMax = 240;
 
-BLEIntCharacteristic bleCharSampleRate(kCharacteristicSampleUUID, BLERead | BLENotify | BLEWrite);
-
+BLEIntCharacteristic bleCharSampleRate(kCharacteristicSampleUUID, BLERead | BLEWrite);
 
 // Fifth Characteristic - Date - date type - a date string of format "YYYY-MM-DD"
 String strDate = "2021-03-01";
-// Note: Setting max value size of kMessage Max  
-BLEStringCharacteristic bleCharDate(kCharacteristicDateUUID, BLERead | BLENotify | BLEWrite, kMessageMax);
+BLEStringCharacteristic bleCharDate(kCharacteristicDateUUID, BLERead | BLEWrite, kMessageMax);
 
 // Sixth Characteristic - Time - time type - a time string of format "HH:MM"
 String strTime = "12:13";
-// Note: Setting max value size of kMessage Max  
-BLEStringCharacteristic bleCharTime(kCharacteristicTimeUUID, BLERead | BLENotify | BLEWrite, kMessageMax);
+BLEStringCharacteristic bleCharTime(kCharacteristicTimeUUID, BLERead | BLEWrite, kMessageMax);
 
-// Seventh Characteristic -Offset - Float type - the offset value
+// Seventh Characteristic -Offset - Float type - the offset value.
+//
+// This value has Notifications enabled.
 // A float property - "offset value"
 float offsetValue = 4.124;
 
@@ -76,8 +90,14 @@ BLEFloatCharacteristic bleCharOffset(kCharacteristicOffsetUUID, BLERead | BLENot
 //--------------- end object setup ------------------------
 const byte LED_TO_TOGGLE = 19;
 
-//---------------------------------------------------------------
-// Callbacks
+//--------------------------------------------------------------------------------------
+// Property changed callback functions.
+//--------------------------------------------------------------------------------------
+// These functions are connected to their respective characteristics and are called
+// when the underlying value is changed.
+//
+// The function signature is defined by Arduino BLE
+
 void enbabledUpdateCB(BLEDevice central, BLECharacteristic theChar){
 
     Serial.print("Is Enabled Update: ");
@@ -95,16 +115,16 @@ void buadRateUpdateCB(BLEDevice central, BLECharacteristic theChar){
     Serial.println(baudRate);
 }
 
-void messageUpdateCB(BLEDevice central, BLECharacteristic theChar){
+void nameUpdateCB(BLEDevice central, BLECharacteristic theChar){
 
     char buffer[kMessageMax+1]={0}; // buffer size + null - zero out 
 
     int len = theChar.valueLength();
     theChar.readValue((void*)buffer, len > kMessageMax ? kMessageMax : len);
 
-    strMessage = buffer;
-    Serial.print("Message Update: ");
-    Serial.println(strMessage);;
+    strName = buffer;
+    Serial.print("Device Name Update: ");
+    Serial.println(strName);;
 }
 void sampleRateUpdateCB(BLEDevice central, BLECharacteristic theChar){
 
@@ -143,8 +163,32 @@ void offsetUpdateCB(BLEDevice central, BLECharacteristic theChar){
     theChar.readValue((void*)&offsetValue, 4);
     Serial.println(offsetValue);
 }
-//---------------------------------------------------------------
-// Wireup the charactritics 
+//--------------------------------------------------------------------------------------
+// Characteristic Setup
+//--------------------------------------------------------------------------------------
+// Connect and setup each characteristic broadcast from this service.
+//
+// >> SparkFun Web-App Configuration <<
+//    
+//   The characteristics are configured to work with the SparkFun BLE Settings Web-App.
+// 
+//   For each characteristic:
+//      - The related SparkFun BLE Property function is called. This adds the BLE Descriptors 
+//        used to define the attributes of this characteristic that the SparkFun BLE app uses
+//      - Add the characteristic to the service
+//      - Set the value of the characteristic to it's associated value variable (declared above)
+//      - Set the value event handler (declared above)
+// 
+//   The setup sequence defines the user experience in the SparkFun BLE Application. 
+//   
+//   Specifically:
+//          - The order that characteritics are setup (calls to sfe_bleprop_ functions),
+//            defines the display order in the app.
+//          - Adding a "Group Title" to a characteristic, causes the app to display this 
+//            title before rendering the property GUI. A group title is just a title, nothing more.
+//   
+//   
+
 void setupBLECharacteristics(BLEService& theService){
 
     // The enabled char
@@ -153,8 +197,8 @@ void setupBLECharacteristics(BLEService& theService){
     bleCharEnabled.setValue(bIsEnabled);
     bleCharEnabled.setEventHandler(BLEWritten, enbabledUpdateCB);
 
-    // Event details group
-    sf_bleprop_group(bleCharDate, "Event Details");    
+    // Add Event Details group title to this char
+    sf_bleprop_group_title(bleCharDate, "Event Details");    
 
     // The date char
     sf_bleprop_date(bleCharDate, "Start Date"); // setup property descriptor            
@@ -168,8 +212,8 @@ void setupBLECharacteristics(BLEService& theService){
     bleCharTime.setValue(strTime);
     bleCharTime.setEventHandler(BLEWritten, timeUpdateCB);  
 
-    // Device Settings group
-    sf_bleprop_group(bleCharBaudRate, "Device Settings");  
+    // Add Device Settings group title to this char
+    sf_bleprop_group_title(bleCharBaudRate, "Device Settings");  
 
     // The BaudRate char
     sf_bleprop_int(bleCharBaudRate, "Baud Rate"); // setup property descriptor    
@@ -177,15 +221,15 @@ void setupBLECharacteristics(BLEService& theService){
     bleCharBaudRate.setValue(baudRate);
     bleCharBaudRate.setEventHandler(BLEWritten, buadRateUpdateCB);
 
-    // The Message char
-    sf_bleprop_string(bleCharMessage, "Device Message"); // setup property descriptor        
-    theService.addCharacteristic(bleCharMessage);  
-    bleCharMessage.setValue(strMessage);
-    bleCharMessage.setEventHandler(BLEWritten, messageUpdateCB);    
+    // The Name char
+    sf_bleprop_string(bleCharName, "Device Name"); // setup property descriptor        
+    theService.addCharacteristic(bleCharName);  
+    bleCharName.setValue(strName);
+    bleCharName.setEventHandler(BLEWritten, nameUpdateCB);    
         
 
-    // Sensor Settings group
-    sf_bleprop_group(bleCharSampleRate, "Sensor Settings");  
+    // Add Sensor Settings group title to this char
+    sf_bleprop_group_title(bleCharSampleRate, "Sensor Settings");  
     // The Sample Rate char
     sf_bleprop_range(bleCharSampleRate, "Sample Rate (sec)", sampleRateMin, sampleRateMax); // setup property descriptor            
     theService.addCharacteristic(bleCharSampleRate);
@@ -199,20 +243,34 @@ void setupBLECharacteristics(BLEService& theService){
     bleCharOffset.setEventHandler(BLEWritten, offsetUpdateCB);  
 
 }
+//-------------------------------------------------------------------------
+// A BLE client (device) is connected logic.
+//-------------------------------------------------------------------------
+//
+// The system is setup to call callback methods to the below object. 
+// A bool is used to keep track of connected state..
+bool deviceConnected = false;
 // General Connect callbacks
 void blePeripheralConnectHandler(BLEDevice central) {
   // central connected event handler
   Serial.print("Connected event, central: ");
   Serial.println(central.address());
+  deviceConnected = true;
 }
 
 void blePeripheralDisconnectHandler(BLEDevice central) {
   // central disconnected event handler
   Serial.print("Disconnected event, central: ");
   Serial.println(central.address());
+  deviceConnected = false;  
 }
-
+// Value used to manage timing for a Notification example in loop.
 unsigned long ticks;
+
+//---------------------------------------------------------------------------------
+// Setup our system
+//---------------------------------------------------------------------------------
+
 void setup() {
 
     // start up serial port
@@ -234,36 +292,40 @@ void setup() {
 
     // name the device
     BLE.setLocalName(kTargetServiceName);
-//    BLE.setAdvertisedService(bleService);    
 
     // Setup service Characteristics
     setupBLECharacteristics(bleService);
 
-    // add the LED service to the BLE device
+    // add the service to the BLE device and start to broadcast.
     BLE.addService(bleService);
     BLE.setAdvertisedService(bleService);
 
     // broadcast BLE connection
     BLE.advertise();
 
-    ticks = millis();
+    ticks = millis(); // for our notify example below
     Serial.println(F("OLA BLE ready for connections!"));
 }
 
 void loop()
 {
-
+    // >> Update and Notification Example <<
+    //
+    // This section updates the offset characterisitc value every 5 secs if a
+    // device is connected. Demostrates how to send a notification using the 
+    // BLE API.
     if(millis() - ticks > 5000){
         // Update the value of offset and set in BLE char
         // Should trigger a notification on client
         offsetValue += .5;
-        bleCharOffset.setValue(offsetValue);
-        Serial.print("Incrementing Offset to: "); Serial.println(offsetValue);
-        
+        if(deviceConnected){
+            bleCharOffset.setValue(offsetValue);
+            Serial.print("Incrementing Offset to: "); Serial.println(offsetValue);
+        }
         ticks = millis();
     }
 
-    // everything is handled in callbacks.
+    // Pump the BLE service - everything is handled in callbacks.
     BLE.poll();
 
 }
